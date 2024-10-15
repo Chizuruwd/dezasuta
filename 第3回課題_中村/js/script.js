e7391efcae7799ad04089a549f0d117b4f79c572
Chizuruwd <sorashia.design@gmail.com>
2024-10-16 01:41:14 +0900

    リセット

--- 第3回課題_中村/js/script.js
+++
@@ -1,9 +0,0 @@
-// work1
-let num = 0;
-for (let i = 1; i <= 10; i++ ) {
-    num = num + i;
-}
-// console.log(num);
-if (num >=20 ) {console.log("20以上");
-}else if (num < 20) {console.log("20未満")
-}
